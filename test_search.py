491c360ae0adb01e9793bafac7881f541ff3062c
soroushfathi <soroush8fathi@gmail.com>
2023-03-23 10:21:25 +0330

    write unittest for limited_dfs

--- test_search.py
+++ test_search.py
@@ -3,6 +3,7 @@ from Problem import Problem
 from State import State
 from Search import Search
 import unittest
+import logging
 
 class TestSearch(unittest.TestCase):
 
@@ -35,3 +36,24 @@ class TestSearch(unittest.TestCase):
         s = Search.bfs(Problem(State(p, None, 0, (0, 0))))
         s.print_path()
         s.execute_gui()
+
+        
+    def test_2_limited_bfs():
+        test_path = 'tests/test2.txt'
+    
+        file = open(test_path, 'r')
+        p = []
+        for i in file.readlines():
+            a = i.replace('\n', '')
+            a = a.replace(' ', '')
+            a = a.split(',')
+            p.append(Pipe(a[:-1], int(a[-1])))
+
+        MAX_DEPTH = 10
+        for i in range(MAX_DEPTH):
+            s = Search.limited_dfs(Problem(State(p, None, 0, (0, 0))), i)
+            try:    
+                s.print_path()
+            except AttributeError:
+                logging.error({f"with depth of {i}, limited dfs has not find solution"})
+        # s.execute_gui()
