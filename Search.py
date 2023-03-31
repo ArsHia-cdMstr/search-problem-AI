41f77f1edcb7a82bfd3b754de623e1685599b00e
soroushfathi <soroush8fathi@gmail.com>
2023-03-31 20:18:14 +0330

    merge up-to-date

--- Search.py
+++ Search.py
@@ -2,8 +2,6 @@ from Solution import Solution
 from Problem import Problem
 from datetime import datetime
 
-import math
-
 
 class Search:
     def __init__(self) -> None:
@@ -135,6 +133,60 @@ class Search:
 
     @staticmethod
     def heuristic_iterative_A_star(prb: Problem) -> Solution:
+        start_time = datetime.now()
+        priority_queue = []
+        explored = {}
+        state = prb.initState
+        priority_queue.append(state)
+        while len(priority_queue) > 0:
+            priority_queue.sort(key=lambda x: x.f_n, reverse=True)
+            state = priority_queue.pop()
+            hashed_state = state.__hash__()
+            explored[hashed_state] = state
+            neighbors = prb.successor(state)
+            for c in neighbors:
+                if prb.is_goal(c):
+                    return Solution(c, prb, start_time)
+                if c.__hash__() not in explored:
+                    priority_queue.append(c)
+        return None
+
+    @staticmethod
+    def RBFS(prb: Problem) -> Solution:
+        explored = {}
+        state = prb.initState
+        explored[state.__hash__()] = state
+
+        return Search.__RBFS_recursive(prb, state, explored, state.h_n_1)
+
+    @staticmethod
+    def __RBFS_recursive(prb: Problem, parent_state, explored, h_value):
+        start_time = datetime.now()
+        children = prb.successor(parent_state)
+        children.sort(key=lambda x: x.h_n_1)
+
+        for j in range(len(children)):
+            state = children[j]
+
+            if state.h_n_1 > h_value:
+                for z in range(j):
+                    st = children[z]
+                    if st.__hash__() in explored:
+                        explored.pop(st.__hash__())
+                return None
+
+            hashed_state = state.__hash__
+            if hashed_state not in explored:
+                if prb.is_goal(state):
+                    return Solution(state, prb, start_time)
+                explored[hashed_state] = state
+
+            s = Search.__RBFS_recursive(prb, state, explored, state.h_n_1)
+            if type(s) == Solution:
+                return s
+
+    @staticmethod
+    def IDA_star(prb: Problem) -> Solution:
         start_time = datetime.now()
         priority_queue = []
         explored = {}
